f6658032b97f8dfa06654101912fb9c610ee3587
Gokay Yilmaz <gokay6320@gmail.com>
2026-07-02 21:19:44 +0300

    Refactor tests to retrieve and validate API tags and articles

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('get test tags', async ({ request }) => {
+  const response = await request.get("https://conduit-api.bondaracademy.com/api/tags/")
+  const responseBody = await response.json()
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  expect(response.status()).toBe(200)
+  expect(responseBody.tags[0]).toBe("Test")
+  expect(responseBody.tags.length).toBeLessThanOrEqual(10)
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+test("get all articler", async ({ request }) => {
+  const response = await request.get("https://conduit-api.bondaracademy.com/api/articles?limit=10&offset=0")
+  const responseBody = await response.json()
+  
+  expect(response.status()).toBe(200)
+  expect(responseBody.articles.length).toBeLessThanOrEqual(10)
+  expect(responseBody.articlesCount).toBeLessThanOrEqual(10)
+})
